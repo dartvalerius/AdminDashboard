e2d91bff66d7bf2423054c5eae892a3196102728
Filippenkov Valeriy <v.filippenkov@defin.by>
2025-06-20 15:21:44 +0300

    Удалён интерфейс репозитория

--- Infrastructure/AD.Persistence/DependencyInjection.cs
+++ Infrastructure/AD.Persistence/DependencyInjection.cs
@@ -12,7 +12,6 @@ public static class DependencyInjection
     public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<ADDbContext>(options => options.UseSqlite(configuration["DbConnection"]));
-        services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
         services.AddScoped<IPasswordHasher, PasswordHasher>();
 
         return services;
